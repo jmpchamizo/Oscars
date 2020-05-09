1cee0eeb8af580c3f0d1587077a3bd7bcdf43a4a
Jose <jmpchamizo@gmail.com>
2020-05-09 17:04:13 +0200

    redimensionado del texto

--- src/console_utils.py
+++ src/console_utils.py
@@ -10,12 +10,12 @@ console_colors = {
     "Reset": "\u001b[0m"
 }
 
-cols = (1, 12, 34, 51, 76, 98)
+cols = (0, 11, 23, 16, 25, 22, 5)
 
 def set_color(color):
     print(f"{console_colors[color]}", end="")
 
-def reset_color(text):
+def reset_color():
     print(f"{console_colors['Reset']}", end="")
 
 def set_position(row, column):
@@ -28,16 +28,28 @@ def print_result(result):
     to_print = result[["year_film", "film", "name", "category", "Genres", "Rate"]]
     temp = [to_print.iloc[i].values for i in range(len(to_print))]
     row = 2
+    current_column = 1
     clear_screen()
     for column,e in enumerate(to_print.columns):
-        set_position(1, cols[column])
+        current_column += cols[column]
+        set_position(1, current_column)
         print(e, end="")
     for i, e in enumerate(temp):
         if result.iloc[i]["win"] == True:
             set_color("Yellow")
         else:
             set_color("Cyan")
+        current_column = 1
         for column, text in enumerate(e):
-            set_position(row, cols[column])
-            print(text, end = "")
+            current_column += cols[column]
+            set_position(row, current_column)
+            text = redim_text(str(text), cols[column+1])
+            print(text)
         row += 1
+
+
+def redim_text(text, num_characters):
+    if len(text) > num_characters-1:
+        return text[:num_characters-4] + "..."
+    return text
+    
