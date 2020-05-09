a74ad80299f8059026522158cbc6dfd23455d23f
Jose <jmpchamizo@gmail.com>
2020-05-09 18:40:35 +0200

    Generación pdf con caratulas de las peliculas

--- src/pdf_utils.py
+++ src/pdf_utils.py
@@ -1,6 +1,13 @@
 from fpdf import FPDF
 import fpdf
 import pandas as pd 
+import requests
+
+
+
+
+URL = "http://image.tmdb.org/t/p/w200/"
+
 
 fpdf.set_global('SYSTEM_TTFONTS', "Fonts")
 
@@ -23,11 +30,12 @@ def create_pdf(oscars):
     pdf.add_page()
     pdf.set_font('NotoSans', 'B', 12)
     for i,_ in enumerate(list(oscars.film)):
-
+        im = get_image(URL + oscars.Poster[i], i)
+        print(URL + oscars.Poster[i])
         if i == 0:
-            cell_film(pdf, "", x_image, y_image + H_CELL*count_cell, H_IMAGE, oscars.film[i], oscars.Description[i], oscars.Rate[i], oscars.win[i], oscars.name[i], oscars.category[i])
+            cell_film(pdf, im, x_image, y_image + H_CELL*count_cell, H_IMAGE, oscars.film[i], oscars.Description[i], oscars.Rate[i], oscars.win[i], oscars.name[i], oscars.category[i])
         else:
-            cell_film(pdf, "", x_image, y_image + (H_CELL+5)*count_cell, H_IMAGE, oscars.film[i], oscars.Description[i], oscars.Rate[i], oscars.win[i], oscars.name[i], oscars.category[i])
+            cell_film(pdf, im, x_image, y_image + (H_CELL+5)*count_cell, H_IMAGE, oscars.film[i], oscars.Description[i], oscars.Rate[i], oscars.win[i], oscars.name[i], oscars.category[i])
         count_cell += 1
         if count_cell == 4:
             count_cell = 0
@@ -38,9 +46,6 @@ def create_pdf(oscars):
 
 
 def cell_film(pdf=None, image="", x_image=15, y_image=15 ,h_image=51, title="",description="", rate="", win=False, name="", category=""):
-    URL = "http://image.tmdb.org/t/p/w200/"
-
-
     if win:
         pdf.set_fill_color(153,153,0)
     else:
@@ -51,13 +56,12 @@ def cell_film(pdf=None, image="", x_image=15, y_image=15 ,h_image=51, title="",d
         pdf = get_fpdf()
     if image == "":
         image = "img/not_found.jpg"
-    else:
-        image = URL + image
+
     pdf.image(image, x_image, y_image, h=h_image, type="jpg")
     #name - category
     na_ca = resize_text(('NotoSans', 'B', 12),f"{name} - {category}",190)
     pdf.set_text_color(0)
-    pdf.text(x_center_text(('NotoSans', 'B', 12), na_ca, 190), y_image-1, na_ca)
+    pdf.text(x_center_text(('NotoSans', 'B', 12), na_ca, 190), y_image, na_ca)
     pdf.set_text_color(50)
     #title
     pdf.text(x_image + 60, y_image+6, f"Titulo: {title}")
@@ -98,8 +102,9 @@ def split_text(font, text, width):
 
 def x_center_text(font, text, width):
     ver = get_fpdf()
+    h_image = 51
     ver.set_font(font[0], font[1], font[2])
-    return 10 + (width - ver.get_string_width(text))/2
+    return 10 + (width - ver.get_string_width(text) + h_image)/2
 
 def resize_text(font, text, width):
     ver = get_fpdf()
@@ -110,3 +115,12 @@ def resize_text(font, text, width):
         text = text[:-7] + "..."
     return text
 
+
+
+def get_image(url, i):
+    image_request = requests.get(url)
+    if image_request.status_code == 200:
+        with open(f"img/image{i}.jpg", 'wb') as f:
+            f.write(image_request.content)
+            return f"img/image{i}.jpg"
+    return ""
