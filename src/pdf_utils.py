b0a960bc093affec8b98cffb1c061e40d05fef76
Jose <jmpchamizo@gmail.com>
2020-05-09 17:43:31 +0200

    Arreglado error al generar pdf

--- src/pdf_utils.py
+++ src/pdf_utils.py
@@ -1,5 +1,17 @@
-from fpdf import FPDF 
+from fpdf import FPDF
+import fpdf
 import pandas as pd 
+
+fpdf.set_global('SYSTEM_TTFONTS', "Fonts")
+
+def get_fpdf():
+    pdf = FPDF()
+    pdf.add_font("NotoSans", style="", fname="NotoSans-Regular.ttf", uni=True)
+    pdf.add_font("NotoSans", style="B", fname="NotoSans-Bold.ttf", uni=True)
+    pdf.add_font("NotoSans", style="I", fname="NotoSans-Italic.ttf", uni=True)
+    pdf.add_font("NotoSans", style="BI", fname="NotoSans-BoldItalic.ttf", uni=True)
+    return pdf
+
 
 def create_pdf(oscars):
     count_cell = 0
@@ -7,9 +19,9 @@ def create_pdf(oscars):
     y_image = 14
     H_IMAGE = 51
     H_CELL = 60
-    pdf = FPDF('P','mm','A4')
+    pdf = get_fpdf()
     pdf.add_page()
-    pdf.set_font('Arial', 'B', 12)
+    pdf.set_font('NotoSans', 'B', 12)
     for i,_ in enumerate(list(oscars.film)):
 
         if i == 0:
@@ -36,23 +48,23 @@ def cell_film(pdf=None, image="", x_image=15, y_image=15 ,h_image=51, title="",d
     pdf.cell(190, 60, "", 1, 1, 'C', True)
     
     if pdf == None:
-        pdf = FPDF('P','mm','A4')
+        pdf = get_fpdf()
     if image == "":
         image = "img/not_found.jpg"
     else:
         image = URL + image
     pdf.image(image, x_image, y_image, h=h_image, type="jpg")
     #name - category
-    na_ca = resize_text(('Arial', 'B', 12),f"{name} - {category}",190)
+    na_ca = resize_text(('NotoSans', 'B', 12),f"{name} - {category}",190)
     pdf.set_text_color(0)
-    pdf.text(x_center_text(('Arial', 'B', 12), na_ca, 190), y_image-1, na_ca)
+    pdf.text(x_center_text(('NotoSans', 'B', 12), na_ca, 190), y_image-1, na_ca)
     pdf.set_text_color(50)
     #title
     pdf.text(x_image + 60, y_image+6, f"Titulo: {title}")
     #rate
     pdf.text(x_image + 60 , y_image+12, f"Puntuación: {rate}")
     #description
-    text = split_text(['Arial', 'B', 12],description,115)
+    text = split_text(['NotoSans', 'B', 12],description,115)
     y = y_image+18
     for i,e in enumerate(text):
         if i < 7:
@@ -67,7 +79,7 @@ def cell_film(pdf=None, image="", x_image=15, y_image=15 ,h_image=51, title="",d
 
 
 def split_text(font, text, width):
-    ver = FPDF()
+    ver = get_fpdf()
     index = 0
     temp = ""
     result = []
@@ -85,12 +97,12 @@ def split_text(font, text, width):
     return result
 
 def x_center_text(font, text, width):
-    ver = FPDF()
+    ver = get_fpdf()
     ver.set_font(font[0], font[1], font[2])
     return 10 + (width - ver.get_string_width(text))/2
 
 def resize_text(font, text, width):
-    ver = FPDF()
+    ver = get_fpdf()
     ver.set_font(font[0], font[1], font[2])
     if ver.get_string_width(text) > width:
         while ver.get_string_width(text) >= width:
